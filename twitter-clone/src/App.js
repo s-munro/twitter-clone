2b1eb1ee42fef18f43fa654ae317762f7a9b39e0
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-24 21:22:43 -0500

    create reat app

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <div></div>
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
