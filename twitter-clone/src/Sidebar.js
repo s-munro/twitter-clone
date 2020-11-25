a78231a545b5d8a22f3998fae81f4ae81a259b3e
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-24 22:21:58 -0500

    sidebar styling

--- twitter-clone/src/Sidebar.js
+++ twitter-clone/src/Sidebar.js
@@ -12,13 +12,15 @@ import ListAltIcon from "@material-ui/icons/ListAlt";
 import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
+import { Button } from "@material-ui/core";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       {/* Twitter icon */}
       <TwitterIcon />
       {/* SidebarOption */}
-      <SidebarOption text="Home" Icon={HomeIcon} />
+      <SidebarOption active text="Home" Icon={HomeIcon} />
       <SidebarOption text="Explore" Icon={SearchIcon} />
       <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
       <SidebarOption text="Messages" Icon={ChatBubbleOutlineIcon} />
@@ -28,6 +30,9 @@ const Sidebar = () => {
       <SidebarOption text="More" Icon={MoreHorizIcon} />
 
       {/* button - tweet */}
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 };
